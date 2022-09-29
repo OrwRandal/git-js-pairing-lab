e8f77ff847ca7d9c4b7ad5643c23be8efa7f5bcd
OrwRandal <randypichardo1987@gmail.com>
2022-09-29 18:03:18 +0000

    i did 1 and 2

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Code your solutions in this file
+function fiveToOneHundred(){
+    for(let i = 5; i<=100; i++){
+        console.log(i)
+    }
+}
+//fiveToOneHundred();
+function multiplesOfThree(){
+    for(let i = 3; i<=100; i+=3){
+        console.log(i)
+    }
+}
+console.log(multiplesOfThree());
+function multiplesOfThreeOrFive(){
+    
+}
