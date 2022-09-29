cca82f06a1a6cbc940d2b86102527ea683d395b7
OrwRandal <randypichardo1987@gmail.com>
2022-09-29 18:59:42 +0000

    Randy and Xhes, with great teamwork, fought our way through all the questions

--- index.js
+++ index.js
@@ -60,3 +60,9 @@ function betweenTwentyAndFourty(num){
     return num < 40 && num > 20;
 }
 //console.log(betweenTwentyAndFourty(21));
+function largest(num1, num2, num3){
+    if(num1 > num2 && num1>num3){return num1}
+    else if(num2 > num1 && num2 > num3){return num2}
+    else{return num3}
+}
+//console.log(largest(4,5,3));
