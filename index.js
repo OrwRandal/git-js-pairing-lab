5969d5115bef962b89783b0f9edfcfcb2d8fe9fd
xhesikam <xhesikam2184@tywls.org>
2022-09-29 18:14:30 +0000

    xhes solved

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ function multiplesOfThree(){
         console.log(i)
     }
 }
-console.log(multiplesOfThree());
+//console.log(multiplesOfThree());
+
 function multiplesOfThreeOrFive(){
+    for(let i =3; i<=100; i++ ){
+        if (i%3 === 0 || i % 5 === 0 ){
+            console.log(i);
+        }
+    }
     
 }
+multiplesOfThreeOrFive();
+
